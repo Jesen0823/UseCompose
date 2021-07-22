43385ced2e43dc9d61b34d3a76fb0289dd97102a
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-22 22:59:15 +0800

    [First Compose] add param 'names'

--- app/src/main/java/com/example/usecompose/MainActivity.kt
+++ app/src/main/java/com/example/usecompose/MainActivity.kt
@@ -43,11 +43,15 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun MyScreenContent(){
+fun MyScreenContent(names:List<String> = listOf("Android","there")){
     Column() {
-        Greeting(name = "Android")
+        for (name in names){
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+        /*Greeting(name = "Android")
         Divider(color = Color.Black)
-        Greeting("there")
+        Greeting("there")*/
     }
 }
 
