2811e488e382e9c2e27c1eea05e7edc7d0181943
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-23 17:13:55 +0800

    [second Compose as module] add button to control the position of LazyColumn by coroutineScope

--- ui_abstraction/src/main/java/com/example/ui_abstraction/ScaffoldActivity.kt
+++ ui_abstraction/src/main/java/com/example/ui_abstraction/ScaffoldActivity.kt
@@ -4,21 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.ui_abstraction.ui.theme.LayoutsCodeTheme
+import kotlinx.coroutines.launch
 
 class ScaffoldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,7 +80,7 @@ fun BodyContent(modifier: Modifier = Modifier) {
 fun SimpleList() {
     val scrollState = rememberLazyListState()
     LazyColumn(state = scrollState) {
-        items(20) {
+        items(10) {
             Text("Item #$it")
         }
     }
@@ -102,8 +108,36 @@ fun ImageListItem(index: Int) {
 // 图文列表
 @Composable
 fun ImageList() {
+    val listSize = 100
+    // We save the scrolling position with this state
     val scrollState = rememberLazyListState()
-
+    // We save the coroutine scope where our animated scroll will be executed
+    // create a CoroutineScope using the rememberCoroutineScope function to create coroutines from the button event handlers
+    val coroutineScope = rememberCoroutineScope()
+    Row(
+        Modifier.padding(5.dp)
+    ){
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            },
+            Modifier.background(Color.Green, RoundedCornerShape(8.dp))
+        ) {
+            Text("Scroll to Top")
+        }
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize -1)
+                }
+            },
+            Modifier.background(Color.Green, RoundedCornerShape(8.dp))
+        ) {
+            Text("Scroll to End")
+        }
+    }
     LazyColumn(state = scrollState) {
         items(100) {
             ImageListItem(index = it)
