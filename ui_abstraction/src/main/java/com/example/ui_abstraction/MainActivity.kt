3e5ffa83931aa5444a0a7c167e904b6e5654d4ab
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-23 11:08:55 +0800

    [second Compose as module] use Modifier

--- ui_abstraction/src/main/java/com/example/ui_abstraction/MainActivity.kt
+++ ui_abstraction/src/main/java/com/example/ui_abstraction/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.ui_abstraction
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ui_abstraction.ui.theme.LayoutsCodeTheme
+import androidx.compose.material.Surface as Surface
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +30,7 @@ class MainActivity : ComponentActivity() {
             LayoutsCodeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-
+                    PhotographerCard()
                 }
             }
         }
@@ -26,11 +38,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PhotographerCard(){
-    Column() {
-        Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
-        CompositionLocalProvider( LocalContentAlpha provides ContentAlpha.medium) {
-            Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+fun PhotographerCard(modifier: Modifier = Modifier){
+    Row(
+        // 链式调用先后顺序不同，效果也会不同
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = {/* onClick*/ })
+            .padding(16.dp)
+    ){
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ){
+            // Image add
+        }
+        Column(
+            // Modifier的链式调用
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider( LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
         }
     }
 }
