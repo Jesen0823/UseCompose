2957be5a469a2411907923a0772202a983250359
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-23 20:20:42 +0800

    [second Compose as module] MyOwnColumn, a simple custom Layout

--- ui_abstraction/src/main/java/com/example/ui_abstraction/CostomLayoutSimple.kt
+++ ui_abstraction/src/main/java/com/example/ui_abstraction/CostomLayoutSimple.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -26,6 +28,39 @@ fun Modifier.firstBaseLineToTop(firstBaseLineToTop: Dp) = this.then(
         }
     }
 )
+
+@Composable
+fun MyOwnColumnLayout(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
+    }
+}
 
 @Preview
 @Composable
--- ui_abstraction/src/main/java/com/example/ui_abstraction/CustomLayoutActivity.kt
+++ ui_abstraction/src/main/java/com/example/ui_abstraction/CustomLayoutActivity.kt
@@ -67,70 +67,12 @@ fun CustomLayoutLab() {
 
 @Composable
 fun BodyContentLay(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(8.dp)) {
-        Text(text = "Hi there")
-        Text(text = "Thanks for going through the Layouts codelab")
-        ImageTextList()
-    }
-}
-
-// 图文列表的Item
-@Composable
-fun ListItem(index: Int) {
-    val imageUrl =
-        "https://dribbble.com/assets/default_avatars/avatar-1-097423283dcf837a2838017f37c1ba97677af3a32e0eadfde1a182ff8f77fc9d.png"
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painter = rememberImagePainter(
-                data = "https://dribbble.com/assets/default_avatars/avatar-1-097423283dcf837a2838017f37c1ba97677af3a32e0eadfde1a182ff8f77fc9d.png"
-
-            ),
-            contentDescription = "Android Logo",
-            modifier = Modifier.size(50.dp)
-        )
-        Spacer(Modifier.width(10.dp))
-        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
-    }
-}
-
-// 图文列表
-@Composable
-fun ImageTextList() {
-    val listSize = 100
-    // We save the scrolling position with this state
-    val scrollState = rememberLazyListState()
-    // We save the coroutine scope where our animated scroll will be executed
-    // create a CoroutineScope using the rememberCoroutineScope function to create coroutines from the button event handlers
-    val coroutineScope = rememberCoroutineScope()
-    Row(
-        Modifier.padding(5.dp)
-    ) {
-        Button(
-            onClick = {
-                coroutineScope.launch {
-                    scrollState.animateScrollToItem(0)
-                }
-            },
-            Modifier.background(Color.Green, RoundedCornerShape(8.dp))
-        ) {
-            Text("Scroll to Top")
+        MyOwnColumnLayout(modifier.padding(8.dp)) {
+            Text("MyOwnColumn")
+            Text("places items")
+            Text("vertically.")
+            Text("We've done it by hand!")
         }
-        Button(
-            onClick = {
-                coroutineScope.launch {
-                    scrollState.animateScrollToItem(listSize - 1)
-                }
-            },
-            Modifier.background(Color.Green, RoundedCornerShape(8.dp))
-        ) {
-            Text("Scroll to End")
-        }
-    }
-    LazyColumn(state = scrollState) {
-        items(100) {
-            ListItem(index = it)
-        }
-    }
 }
 
 @Preview
