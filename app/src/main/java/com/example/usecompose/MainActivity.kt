8cd146a40cae56bf396dba28e389786888726532
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-23 09:50:52 +0800

    [First Compose] change background color of item. animateColorAsState takes a Color as a parameter, saves it and generates automatically the intermediate colors required to display an animated transition from the previously set color to the new one

--- app/src/main/java/com/example/usecompose/MainActivity.kt
+++ app/src/main/java/com/example/usecompose/MainActivity.kt
@@ -3,15 +3,16 @@ package com.example.usecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,7 +42,14 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    var isSelected by remember { mutableStateOf(false) }
+    val bgColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier.padding(24.dp)
+            .background(color = bgColor)
+            .clickable(onClick = {isSelected = !isSelected})
+    )
 }
 
 // it 就是names中的元素
