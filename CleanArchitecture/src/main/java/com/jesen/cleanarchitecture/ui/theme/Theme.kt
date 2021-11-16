985de6c6497ed89a6f984fec5fa5c4e0a32c3d43
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-11-16 23:22:27 +0800

    [ Compose as module] 2. 主题与颜色

--- CleanArchitecture/src/main/java/com/jesen/cleanarchitecture/ui/theme/Theme.kt
+++ CleanArchitecture/src/main/java/com/jesen/cleanarchitecture/ui/theme/Theme.kt
@@ -5,9 +5,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray,
     primaryVariant = Purple700,
     secondary = Teal200
 )
@@ -36,7 +41,7 @@ fun UseComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
     }
 
     MaterialTheme(
-        colors = colors,
+        colors = DarkColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
